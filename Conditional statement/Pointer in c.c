92f69eda8a670b11fb81a3a3f8be70cb6b511558
kavi-n-2110 <e24ai069@shanmugha.edu.in>
2026-02-04 14:11:34 +0530

    Create Pointer in c.c

---
+++ Conditional statement/Pointer in c.c
@@ -0,0 +1,16 @@
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n",a+b);
+    
+    if(a>b)
+    {
+        printf("%d",a-b);
+    }
+    else 
+    {
+        printf("%d",b-a);
+    }
+    
+    return 0;
+}
